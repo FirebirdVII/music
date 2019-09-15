8a5e7268823f523d4968620469d075ffb4a98a7d
FirebirdVII <simon.meyer@stud.uni-bamberg.de>
2019-09-15 12:15:20 +0200

    Aktualisierung

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $(".type-youtube").append('<span class="badge badge-youtube">YT</span>');
+    $(".type-youtube-playlist").append('<span class="badge badge-youtube-playlist">YT-PL</span>');
     $(".type-spotify").append('<span class="badge badge-spotify">S</span>');
 
 
     $(".badge-youtube").css({ "background-color": "#fb5050", "color": "white", "float": "right" });
+    $(".badge-youtube-playlist").css({ "background-color": "#af0000", "color": "white", "float": "right" });
     $(".badge-spotify").css({ "background-color": "#76b852", "color": "white", "float": "right" });
 });
